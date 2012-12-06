083528798503a6d0cc9cda0b6b8c2119446d1969
Milton <mbaltazar.v7@gmail.com>
2012-12-06 04:26:49 -0500

    Clases para las transacciones de roles

--- BENEDICTOXVI/src/benedictoxvi/Entidades/Modulo.java
+++ BENEDICTOXVI/src/benedictoxvi/Entidades/Modulo.java
@@ -12,7 +12,25 @@ public class Modulo {
     private String codigoModulo;
     private String nombreModulo;
     private String descripcionModulo;
+    private boolean acceso;
+    private boolean adicionar;
+    private boolean editar;
+    private boolean eliminar;
 
+    public Modulo()
+    {
+    }
+    public Modulo(String pCodigoModulo, String pNombreModulo, String pDescripcion, boolean pAcceso, boolean pAdicionar, boolean pEditar, boolean pEliminar)
+    {
+        this.codigoModulo = pCodigoModulo;
+        this.nombreModulo = pNombreModulo;
+        this.descripcionModulo = pDescripcion;
+        this.acceso = pAcceso;
+        this.adicionar = pAdicionar;
+        this.editar = pEditar;
+        this.eliminar = pEliminar;
+    }
+    
     public String getCodigoModulo() {
         return codigoModulo;
     }
@@ -35,6 +53,38 @@ public class Modulo {
 
     public void setDescripcionModulo(String descripcionModulo) {
         this.descripcionModulo = descripcionModulo;
+    }
+
+    public boolean isAcceso() {
+        return acceso;
+    }
+
+    public void setAcceso(boolean acceso) {
+        this.acceso = acceso;
+    }
+
+    public boolean isAdicionar() {
+        return adicionar;
+    }
+
+    public void setAdicionar(boolean adicionar) {
+        this.adicionar = adicionar;
+    }
+
+    public boolean isEditar() {
+        return editar;
+    }
+
+    public void setEditar(boolean editar) {
+        this.editar = editar;
+    }
+
+    public boolean isEliminar() {
+        return eliminar;
+    }
+
+    public void setEliminar(boolean eliminar) {
+        this.eliminar = eliminar;
     }
     
     
